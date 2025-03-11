06665df673d8cd1b29403116da0f3b111543bdc9
M. Ihsan Rizqullah Adfa <159456948+ihsanadfa@users.noreply.github.com>
2025-03-12 02:53:13 +0700

    Update server.js
    Added more data

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ app.use(express.json());
 let f1Drivers = [
   {
     id: 1,
-    name: "Lewis Hamilton",
-    team: "Ferrari",
-    country: "United Kingdom",
-    championships: 7,
-    wins: 103
-  },
-  {
-    id: 2,
     name: "Max Verstappen",
     team: "Red Bull Racing",
     country: "Netherlands",
@@ -25,12 +17,156 @@ let f1Drivers = [
     wins: 57
   },
   {
+    id: 2,
+    name: "Sergio Perez",
+    team: "Red Bull Racing",
+    country: "Mexico",
+    championships: 0,
+    wins: 6
+  },
+  {
     id: 3,
+    name: "Lewis Hamilton",
+    team: "Mercedes",
+    country: "United Kingdom",
+    championships: 7,
+    wins: 103
+  },
+  {
+    id: 4,
+    name: "George Russell",
+    team: "Mercedes",
+    country: "United Kingdom",
+    championships: 0,
+    wins: 1
+  },
+  {
+    id: 5,
     name: "Charles Leclerc",
     team: "Ferrari",
     country: "Monaco",
     championships: 0,
     wins: 5
+  },
+  {
+    id: 6,
+    name: "Carlos Sainz",
+    team: "Ferrari",
+    country: "Spain",
+    championships: 0,
+    wins: 2
+  },
+  {
+    id: 7,
+    name: "Lando Norris",
+    team: "McLaren",
+    country: "United Kingdom",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 8,
+    name: "Oscar Piastri",
+    team: "McLaren",
+    country: "Australia",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 9,
+    name: "Fernando Alonso",
+    team: "Aston Martin",
+    country: "Spain",
+    championships: 2,
+    wins: 32
+  },
+  {
+    id: 10,
+    name: "Lance Stroll",
+    team: "Aston Martin",
+    country: "Canada",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 11,
+    name: "Pierre Gasly",
+    team: "Alpine",
+    country: "France",
+    championships: 0,
+    wins: 1
+  },
+  {
+    id: 12,
+    name: "Esteban Ocon",
+    team: "Alpine",
+    country: "France",
+    championships: 0,
+    wins: 1
+  },
+  {
+    id: 13,
+    name: "Valtteri Bottas",
+    team: "Alfa Romeo",
+    country: "Finland",
+    championships: 0,
+    wins: 10
+  },
+  {
+    id: 14,
+    name: "Zhou Guanyu",
+    team: "Alfa Romeo",
+    country: "China",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 15,
+    name: "Kevin Magnussen",
+    team: "Haas",
+    country: "Denmark",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 16,
+    name: "Nico Hulkenberg",
+    team: "Haas",
+    country: "Germany",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 17,
+    name: "Yuki Tsunoda",
+    team: "AlphaTauri",
+    country: "Japan",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 18,
+    name: "Daniel Ricciardo",
+    team: "AlphaTauri",
+    country: "Australia",
+    championships: 0,
+    wins: 8
+  },
+  {
+    id: 19,
+    name: "Alexander Albon",
+    team: "Williams",
+    country: "Thailand",
+    championships: 0,
+    wins: 0
+  },
+  {
+    id: 20,
+    name: "Logan Sargeant",
+    team: "Williams",
+    country: "United States",
+    championships: 0,
+    wins: 0
   }
 ];
 
